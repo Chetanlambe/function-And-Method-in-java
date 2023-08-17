5665ccfc6b6c9964304fa694064611380c235766
Chetan Lambe <138874931+Chetanlambe@users.noreply.github.com>
2023-08-17 17:24:05 +0530

    sum of the two  number
    calculate the sum of the two number

---
+++ calculateSum.java
@@ -0,0 +1,19 @@
+package fuctionAndMethod;
+
+import java.util.Scanner;
+public class calculateSum {
+    public static int sumCalculate(int a , int b){
+        int sum = a + b;
+        return sum;
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the first number");
+        int a = sc.nextInt();
+        System.out.println("Enter the second number");
+        int b = sc.nextInt();
+
+        int sum = sumCalculate(a , b);
+        System.out.println("Sum of the two number is : "+sum);
+    }
+}
